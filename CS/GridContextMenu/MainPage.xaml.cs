fbb64994b56669ad3068f50829fb465fa6daf828
Andrey Marten <andrey.kunitsyn@devexpress.com>
2020-01-12 15:44:48 +0300

    first commit

---
+++ CS/GridContextMenu/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using DevExpress.UI.Xaml.Grid;
+using Windows.UI.Xaml.Controls;
+
+namespace GridContextMenu
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
