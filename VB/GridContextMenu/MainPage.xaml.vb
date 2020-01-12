852ae3de9f797259feb10a57c2af00431724d1d4
DevExpressExampleBot <fakeEmail>
2020-01-12 15:52:12 +0300

    autogenerate VB

---
+++ VB/GridContextMenu/MainPage.xaml.vb
@@ -0,0 +1,12 @@
+﻿Imports DevExpress.UI.Xaml.Grid
+Imports Windows.UI.Xaml.Controls
+
+Namespace GridContextMenu
+	Public NotInheritable Partial Class MainPage
+		Inherits Page
+
+		Public Sub New()
+			Me.InitializeComponent()
+		End Sub
+	End Class
+End Namespace
---
+++ VB/GridContextMenu/MainViewModel.vb
@@ -0,0 +1,89 @@
+﻿Imports DevExpress.Mvvm
+Imports System
+Imports System.Collections.ObjectModel
+Imports System.Linq
+
+Namespace GridContextMenu
+	Public Class MainViewModel
+		Inherits ViewModelBase
+
+		Public Sub New()
+			Me.DeleteCommand = New DelegateCommand(Of Item)(AddressOf Delete)
+			Me.DuplicateCommand = New DelegateCommand(Of Item)(AddressOf Duplicate)
+		End Sub
+
+		Protected _Items As ObservableCollection(Of Item)
+
+		Public ReadOnly Property Items() As ObservableCollection(Of Item)
+			Get
+				If Me._Items Is Nothing Then
+					Me._Items = New ObservableCollection(Of Item)()
+					Dim r As New Random()
+					Dim i As Integer = 0
+					Do While i < 150
+						Dim value As New Item()
+						value.Name = String.Format("Name #{0}", i)
+						value.Age = r.Next(40) + 20
+						value.DateTime = Date.Today.AddDays(r.Next(30) - 15)
+						value.IsSelected = r.Next(2) > 0
+						Me._Items.Add(value)
+						i = i + 1
+					Loop
+				End If
+				Return Me._Items
+			End Get
+		End Property
+
+
+		Protected _SelectedItem As Item
+		Public Property SelectedItem() As Item
+			Get
+				Return Me._SelectedItem
+			End Get
+			Set(ByVal value As Item)
+				Me.SetProperty(Me._SelectedItem, value, "SelectedItem")
+			End Set
+		End Property
+
+		Private privateDeleteCommand As DelegateCommand(Of Item)
+		Public Property DeleteCommand() As DelegateCommand(Of Item)
+			Get
+				Return privateDeleteCommand
+			End Get
+			Private Set(ByVal value As DelegateCommand(Of Item))
+				privateDeleteCommand = value
+			End Set
+		End Property
+
+		Public Sub Delete(ByVal item As Item)
+			If Me.Items.Contains(item) Then
+				Dim index = Me.Items.IndexOf(item)
+				Me.Items.Remove(item)
+				Me.SelectedItem = If(Me.Items.Count < index, Me.Items(index), Me.Items.FirstOrDefault())
+			End If
+
+		End Sub
+
+		Private privateDuplicateCommand As DelegateCommand(Of Item)
+		Public Property DuplicateCommand() As DelegateCommand(Of Item)
+			Get
+				Return privateDuplicateCommand
+			End Get
+			Private Set(ByVal value As DelegateCommand(Of Item))
+				privateDuplicateCommand = value
+			End Set
+		End Property
+
+		Public Sub Duplicate(ByVal item As Item)
+			Dim index = Me.Items.IndexOf(item)
+			Dim newItem = New Item() With {
+				.Name = item.Name,
+				.Age = item.Age,
+				.DateTime = item.DateTime,
+				.IsSelected = item.IsSelected
+			}
+			Me.Items.Insert(index + 1, newItem)
+			Me.SelectedItem = newItem
+		End Sub
+	End Class
+End Namespace
